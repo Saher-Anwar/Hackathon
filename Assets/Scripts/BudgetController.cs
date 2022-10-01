b1540d092b0f0934c14399b068bac56fd989c6a4
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-01 19:17:09 -0300

    - implemented budget controller

---
+++ Assets/Scripts/BudgetController.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BudgetController : MonoBehaviour
+{
+    public float budget = 100;
+    public TextMeshProUGUI tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp.text = $"Budget: ${budget}";
+    }
+
+    public void AddProfit(float profit)
+    {
+        budget += profit;
+    }
+
+    public void SubtractLoss(float loss)
+    {
+        budget -= loss;
+    }
+}
