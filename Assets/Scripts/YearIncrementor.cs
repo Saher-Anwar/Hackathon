09c9d3e3ce0d0ec495ec6b76c4af2daa55b587f0
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-01 19:02:13 -0300

    - imported TMP - added year incrementor file to update year and quarter over time

---
+++ Assets/Scripts/YearIncrementor.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YearIncrementor : MonoBehaviour
+{
+    public int Year = 2022;
+    public int Quarter = 0;
+    public TextMeshProUGUI tmp;
+    public float timeLimitPerQuarterInSeconds = 3;
+
+    private float timeElapsedInSeconds = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsedInSeconds += Time.deltaTime;
+
+        if(timeElapsedInSeconds > timeLimitPerQuarterInSeconds)
+        {
+            Quarter++;
+
+            if(Quarter > 4)
+            {
+                Quarter = 0;
+                Year++;
+            }
+
+            timeElapsedInSeconds = 0;
+        }
+
+        tmp.text = $"Year {Year}\nQuarter {Quarter}";
+
+        
+    }
+}
