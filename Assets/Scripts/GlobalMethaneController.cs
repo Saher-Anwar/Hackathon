516c84bc62ffdb04058b114e03da8d3ab75aed3f
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-01 19:58:26 -0300

    - implemented global methane controller

---
+++ Assets/Scripts/GlobalMethaneController.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalMethaneController : MonoBehaviour
+{
+    public TextMeshProUGUI tmp;
+    public float MethaneLevel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp.text = $"Global CH4: {MethaneLevel}%";   
+    }
+}
