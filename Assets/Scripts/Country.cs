b36e0a4866b9eab519bb22418bf966daa4c2518f
OkudaHeishi <92018284+OkudaHeishi@users.noreply.github.com>
2022-10-01 21:53:56 -0300

    create LNG

--- Assets/Scripts/Country.cs
+++ Assets/Scripts/Country.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Country : MonoBehaviour
 {
     public string CountryName;
+    public GameObject factory;
     public List<GameObject> factories;
 
     // Start is called before the first frame update
@@ -22,5 +23,19 @@ public class Country : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Click detected");
+    }
+
+    public void createNewFactory()
+    {
+        Instantiate(factory, Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.rotation);
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("Constructing LNG factory");
+            createNewFactory();
+        }
     }
 }
