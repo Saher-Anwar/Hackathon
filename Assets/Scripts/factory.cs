d75ebef11aa86ee13335c233ef9051d20da51ca7
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-02 00:32:08 -0300

    - Added time management code to Factory script

--- Assets/Scripts/factory.cs
+++ Assets/Scripts/factory.cs
@@ -10,6 +10,9 @@ public class factory : MonoBehaviour
 
     public GameManager manager;
 
+    private float timeElapsed = 0;
+    public float timeIntervalInSecondsBeforeAddingProfit = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,15 @@ public class factory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // time elapsed keep the track of how much time has passed so far
+        timeElapsed += Time.deltaTime;
+
+        // if the time elapsed is greater than the time that we specified, we add a certain profit
+        if(timeElapsed > timeIntervalInSecondsBeforeAddingProfit)
+        {
+            // add x amount of profit
+        }
+        
         manager.globalMethanecontroller.addEmissions(emission*Time.deltaTime);
         manager.BudgetController.AddProfit(profit*Time.deltaTime);
     }
